0eba15d355b86d47badc1d644688b29c2e2e19f2
Kushan M <kushaaanm@gmail.com>
2024-03-20 19:00:51 +0530

    Update digit_recognizer.py
    changes in defining the model

--- model/digit_recognizer.py
+++ model/digit_recognizer.py
@@ -19,19 +19,13 @@ X_train = X_train.values.reshape(-1, 28, 28, 1)
 # Define the model
 def build_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 1)))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(10, activation='softmax'))
 
     model.compile(optimizer='adam',
