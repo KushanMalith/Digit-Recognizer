349b42a94b2b5fee78aaf78fb02b22f11a9e35f3
Kushan M <kushaaanm@gmail.com>
2024-03-21 06:59:32 +0530

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // To check if there's a predicted digit
     const predictedDigit = document.querySelector('#predicted-digit');
 
-    // If there's a predicted digit, reload the page after 5 seconds
-    if (predictedDigit.innerText !== 'Predicted Digit: ') {
+    // Check if predicted digit element exists and its innerText is not empty
+    if (predictedDigit && predictedDigit.innerText.trim() !== 'Predicted Digit:') {
+        // Reload the page after 5 seconds
         setTimeout(() => {
             window.location.reload();
         }, 5000); // 5000 milliseconds = 5 seconds
