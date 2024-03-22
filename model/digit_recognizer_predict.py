372ae84a4c48ce70732c7a54a8f34287dc08f85a
Kushan M <kushaaanm@gmail.com>
2024-03-22 18:38:21 +0530

    Update digit_recognizer_predict.py

--- model/digit_recognizer_predict.py
+++ model/digit_recognizer_predict.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import load_model
 
 def predict_digit(image_data):
     # Load trained model
-    model = load_model('model/digit_recognizer_model.keras')
+    model = load_model('model/digit_recognizer_model.h5')
     # Reshape image data within the function to match model input shape
     image_data = image_data.reshape(-1, 28, 28, 1)
     # Predict digit probabilities
